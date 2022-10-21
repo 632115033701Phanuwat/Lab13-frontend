001dd54cbe91888a208f20d155eb5fc9d43d5f9d
Phanuwat_emotin <phanuwat_panya@elearning.cmu.ac.th>
2022-10-21 10:37:30 +0700

    fix ip

--- src/services/AxiosClient.js
+++ src/services/AxiosClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: 'http://3.82.204.86:8999',
+  baseURL: 'http://54.164.28.255:8999',
   withCredentials: false,
   headers: {
     Accept: 'application/json',
